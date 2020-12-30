7802ad81283cfefe5ef91e075205e483e2eb8abb
Lasse05 <34139261+Lasse05@users.noreply.github.com>
2020-12-30 16:36:56 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+PACKAGES="python-bluez bluetooth python-pip"
+sudo apt-get update
+sudo apt-get install $PACKAGES -y
+pip install  paho-mqtt
 sudo service beta stop
 sudo rm -r ../scanner
 sudo mv  ../tmp/ ../scanner/
