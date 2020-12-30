bdf06cc2aec81656315678949aca94b4fadd0594
Lasse05 <34139261+Lasse05@users.noreply.github.com>
2020-12-30 16:50:33 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,8 +3,8 @@ PACKAGES="python-bluez bluetooth python-pip"
 sudo apt-get update
 sudo apt-get install $PACKAGES -y
 pip install  paho-mqtt
-sudo python ../scanner/setup.py install
-sudo cp ../scanner/beta.service  /lib/systemd/system/
+sudo python setup.py install
+sudo cp beta.service  /lib/systemd/system/
 sudo chmod 644 /lib/systemd/system/beta.service
 sudo systemctl daemon-reload
 sudo systemctl enable beta.service
